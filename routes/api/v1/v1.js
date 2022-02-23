81e1c2cf13240e00a2814ee179a98dfeb70a96e6
Daniel Rueda <78886794+DaniloRF22@users.noreply.github.com>
2022-02-22 18:53:12 -0600

    Clase_Mar_22/02/2022

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const pacientesRoutes = require('./pacientes/pacientes');
 const expedientesRoutes = require('./expedientes/expedientes');
+const seguridadRoutes = require('./seguridad/seguridad');
 const { verifyApiHeaderToken } = require('./headerVerifyMiddleware');
 
 router.use('/pacientes',verifyApiHeaderToken, pacientesRoutes);
 router.use('/expedientes',verifyApiHeaderToken, expedientesRoutes);
+router.use('/seguridad',verifyApiHeaderToken, seguridadRoutes);
 
 module.exports= router;
